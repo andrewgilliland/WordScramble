d50b5ad7803e1d86a24ba997c5a864cfb203290c
Andrew Gilliland <andrew.gilliland@csm.cc>
2023-05-04 07:41:12 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Andrew Gilliland on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
